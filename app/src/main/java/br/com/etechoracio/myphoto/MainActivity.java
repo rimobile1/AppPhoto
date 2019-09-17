a4f6fe274728801469d549f60b268a9e495ed36f
rimobile1 <icaro.mo1234@gmail.com>
2019-09-16 22:06:40 -0300

    app photo com visualizar funcionando

--- app/src/main/java/br/com/etechoracio/myphoto/MainActivity.java
+++ app/src/main/java/br/com/etechoracio/myphoto/MainActivity.java
@@ -64,6 +64,12 @@ public class MainActivity extends AppCompatActivity {
 
     public  void onVisualizar(View view){
 
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        intent.setDataAndType(uri,"image/jpeg");
+
+        startActivity(intent);
+
 
     }
 }
